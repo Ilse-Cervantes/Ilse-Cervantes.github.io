8aaa5fbf5b4932ad8a28a17cbf13bcac5963a1ba
Ilse Cervantes <144073262+Ilse-Cervantes@users.noreply.github.com>
2023-09-23 13:51:45 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+  cursor: '<span style="color:black;"></span>'
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color:black;">I am only girl</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+  
